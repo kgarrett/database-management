952888a2b50a40b5d551bd5fe42cc7786a8d3c5b
Kelly Garrett <kelly.garrett@utexas.edu>
2017-03-07 11:10:07 -0600

    Create capitalsanswer.php

---
+++ assignment-5/capitalsanswer.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+
+<title>States and Capitals</title>
+
+</head>
+
+<body>
+
+<h1>The capital is ...</h1>
+
+<?php
+
+ 
+
+$capital = $_GET['select'];
+
+ 
+
+print "<p><b>$capital</b></p>"
+
+ 
+
+?>
+
+</body>
+
+</html>
